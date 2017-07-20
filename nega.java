704e75cb6c375b27ff9bc90fef419331674dc3a7
praveena1996 <praveenaselva20@gmail.com>
2017-07-20 18:06:39 +0530

    Create nega.java

---
+++ nega.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class nega
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n>0)
+{
+System.out.print("positive");
+}
+else
+{
+System.out.print("negative");
+}
+}
+}
